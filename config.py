56a246f8cd28fd2fd1eac593faafcd5aec6e6680
gmthubmadharchod <sonusinghtomar778@gmail.com>
2026-02-07 19:24:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,14 +15,14 @@ ADMINS = int(os.environ.get("ADMINS", ""))
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "idfinderpro")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 # Force Subscription Channel
 FORCE_SUB_CHANNEL = "idfinderpro"  # Channel username without @
-FORCE_SUB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441460670"))
+FORCE_SUB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 # Log Channel - Set to 0 to disable logging
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1003439064088"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", ""))
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
